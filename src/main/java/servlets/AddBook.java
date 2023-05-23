972f48731c7ab464247e5424bf15d19c2c221cf0
21700340JuwonBaek <root@DESKTOP-2TH44RN.localdomain>
2023-05-23 17:10:20 +0900

    Add Servlet

--- src/main/java/servlets/AddBook.java
+++ src/main/java/servlets/AddBook.java
@@ -1,11 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.BookDAO;
+import vo.Book;
 
 @WebServlet("/AddBook")
 public class AddBook extends HttpServlet {
@@ -22,8 +27,22 @@ public class AddBook extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
 		request.setCharacterEncoding("UTF8");
 		response.setContentType("text/html;charset=utf8");
-		String id = request.getParameter("id");
 		String title = request.getParameter("title");
+		String author = request.getParameter("author");
+		String writtenDateStr = request.getParameter("writtenDate");
+		Date writtenDate = Date.valueOf(writtenDateStr);
+		String company = request.getParameter("company");
+		int price = Integer.parseInt(request.getParameter("price"));
+		String category = request.getParameter("category");
+		int remain = Integer.parseInt(request.getParameter("remain"));
+		int saledNum = Integer.parseInt(request.getParameter("saledNum"));
+		
+		Book book = new Book(-1, title, author, writtenDate, company, price, category, remain, saledNum); 
+		
+		BookDAO bookDao = new BookDAO();
+		bookDao.addBook(book);
+		
+		response.sendRedirect("main");
 	}
 
 }
